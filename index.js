811d48a65ed0f85b5294e998cba55d2f02defbd3
Map4che <ljuan648l@gmail.com>
2023-07-22 15:05:28 -0500

    Mejorando el codigo, implementando getElementByClassName con ciclo for para cambio de clase

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const btn_hamburguer = document.querySelector(".container-hamburguer");
-const prueba = document.getElementsByClassName("lista-item__link");
-const palabra1 = document.querySelector(".opcion1");
-const palabra2 = document.querySelector(".opcion2");
-const palabra3 = document.querySelector(".opcion3");
-const palabra4 = document.querySelector(".opcion4");
 const menu_image = document.querySelector(".container-hamburguer");
 const menu_hamburguer = document.querySelector(".lista-navbar");
+const palabras_menu = document.getElementsByClassName("lista-item__link");
 
 const cambioDeMenu = () => {
   menu_hamburguer.classList.toggle("aparicion-menu");
   menu_image.classList.toggle("cambio");
-  palabra1.classList.toggle("aparece");
-  palabra2.classList.toggle("aparece");
-  palabra3.classList.toggle("aparece");
-  palabra4.classList.toggle("aparece");
-  prueba.forEach((element) => {
-    prueba.classList.add("probando");
-  });
+
+  for (let i = 0; i < palabras_menu.length; i++) {
+    palabras_menu[i].classList.toggle("aparece");
+  }
 };
 
 btn_hamburguer.addEventListener("click", cambioDeMenu);
